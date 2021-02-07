38b56f0b9cf39cb0dab411adbdab2e36146ee942
pierstoyanov <pier.stoyanov@gmail.com>
2021-02-07 22:40:47 +0200

    +useful code & snippet fix

--- js_snippets.js
+++ js_snippets.js
@@ -1,4 +1,26 @@
+// sort numbers in asc or dsc order
 const compareNumbers = {
     ascending: (a, b) => a - b,
     descending: (a, b) => b - a,
   };
+
+
+  // Quickly turn str characters to math operators
+  // mathOperators[string](a, b)
+  let mathOperators = {
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '*': (a, b) => a * b,
+    '/': (a, b) => a / b,
+};
+
+
+// This removes unsafe characters from HTML text
+function escapeHtml(unsafe) {
+  return unsafe
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
